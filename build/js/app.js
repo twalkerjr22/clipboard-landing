43c2af411d69420064a7604e6b239c6808a9e53d
Troy Walker <twalkerjr22@gmail.com>
2019-07-17 08:43:49 -0400

    - Removed jquery link tag

--- build/js/app.js
+++ build/js/app.js
@@ -6,20 +6,13 @@ let val = landingpage.init();
 },{"./landingpage":2}],2:[function(require,module,exports){
 var jQuery = require("jquery");
 var scrollify = require("jquery-scrollify");
-window.$ = window.jQuery;
+
 var landingPage = {
   init: function init() {
     var sections = jQuery(".full-height");
 
     var sectionsArr = jQuery.makeArray(sections);
-    var prevPosition = 0;
-    scrollify.move(".footer");
-    jQuery(document).on("scroll", function() {
-      let delta = Math.abs(jQuery(window).scrollTop() - prevPosition);
-      console.log("Delta: " + delta);
-
-       
-    });
+    console.log(sectionsArr);
   }
 };
 
